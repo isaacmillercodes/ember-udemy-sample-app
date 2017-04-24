e34a552054fe91e3f3272d0a0dc4c6700a0399ee
Isaac McGaha Miller <isaacmcgahamiller@gmail.com>
2017-04-24 14:08:50 -0600

    finished section 3

--- app/routes/services.js
+++ app/routes/services.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function() {
-    return ['PC Repair', 'Network Diagnostics']
+    return ['PC Repair', 'Network Diagnostics', 'Home Audio Installations'];
   }
 });
